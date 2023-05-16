cb593262ec1995888cff694fb4d8b36aace80942
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2023-05-16 20:57:15 +0100

    --> FP: Aula 04
    - Todos os exercícios da aula resolvidos

--- 1ª Matrícula/1º Semestre/Fundamentos de Programação/Aulas Práticas/Aulas/Aula 04/table.py
+++ 1ª Matrícula/1º Semestre/Fundamentos de Programação/Aulas Práticas/Aulas/Aula 04/table.py
@@ -1,7 +1,7 @@
 # Show a table of the squares of the first four numbers
-print("{:2s} {:2s}".format("n", "n²"))
-for n in [1, 2, 3, 4]:
-    print("{:2d} {:2d}".format(n, n**2))
+print(" {:1s} {:>3s} {:>7s}".format("n", "n²", "2**n"))
+for n in range(1, 21):
+    print("{:2d} {:3d} {:7d}".format(n, n**2, 2**n))
 
 # Modify the program to show the squares of 1..20.  (Use the range function.)
 # Also, add a column to show 2**n.  Adjust the formatting.
--- 1ª Matrícula/1º Semestre/Fundamentos de Programação/Aulas Práticas/Aulas/Aula 04/turtle1.py
+++ 1ª Matrícula/1º Semestre/Fundamentos de Programação/Aulas Práticas/Aulas/Aula 04/turtle1.py
@@ -16,8 +16,17 @@ def square(t, side):
 # The first side should have length = start, the second start+incr, etc.,
 # until the length reaches length=end (exclusive).
 def spiral(t, start, end, incr):
-    # Complete the function...
-    ...
+    if start < end:
+        while start <= end:
+            t.forward(start)
+            t.left(90)
+            start += incr
+    else:
+        while start >= end:
+            t.forward(start)
+            t.left(90)
+            start += incr
+    
 
 def main():
     print("This program opens a window with a graphical user interface.")
@@ -25,27 +34,32 @@ def main():
     alex = turtle.Turtle()      # create a turtle named alex
 
     alex.forward(150)           # tell alex to move forward by 150 units
-    alex.left(90)               # turn by 90 degrees
+    alex.right(90)               # turn by 90 degrees
     alex.forward(75)            # complete the second side
 
     beth = turtle.Turtle()      # another turtle
     beth.shape("turtle")        # with another shape
     beth.color("blue")          # and color
     beth.up()                   # pen up
-    beth.goto(-200, 0)          # move to given point
+    beth.goto(-200, -100)       # move to given point
     beth.down()                 # pen down
     square(beth, 100)           # draw a square
 
-    # Move alex to another place
+    # This should draw a spiral
     alex.up()
-    alex.goto(-200, -200)
+    alex.goto(-200, 200)
     alex.setheading(0)
     alex.down()
-    # This should draw a spiral
     spiral(alex, 10, 200, 10)
+    alex.up()
+    alex.goto(100, 100)
+    alex.setheading(0)
+    alex.down()
+    spiral(alex, 200, 0, -5)
 
     turtle.exitonclick()        # wait for a button click, then close window
     print("The window was closed. Bye!")
 
 
-main()
+if __name__ == "__main__":
+    main()
